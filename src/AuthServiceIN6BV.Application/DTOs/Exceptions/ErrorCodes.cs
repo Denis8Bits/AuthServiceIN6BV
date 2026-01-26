9c99fe60ad47266ccc32d89071219e29d2736c4e
Informatica <informatica@alumno.local>
2026-01-26 17:19:13 -0600

    Subida 2da parte

---
+++ src/AuthServiceIN6BV.Application/DTOs/Exceptions/ErrorCodes.cs
@@ -0,0 +1,12 @@
+namespace AuthService.Application.Exceptions;
+
+public static class ErrorCodes
+{
+    public const string EMAIL_ALREADY_EXISTS = "EMAIL_ALREADY_EXISTS";
+    public const string USERNAME_ALREADY_EXISTS = "USERNAME_ALREADY_EXISTS";
+    public const string INVALID_CREDENTIALS = "INDALID_CREDENTIALS";
+    public const string USER_ACCOUNT_DISABLE = "USER_ACCOUNT_DISABLE";
+    public const string IMAGE_UPLOAD_FAILED = "IMAGE_UPLOAD_FAILED";
+    public const string INVALID_FILE_FORMAT = "INVALID_FILE_FORMAT";
+    public const string FILE_TOO_LARGE = "FILE_TOO_LARGE";
+}
---
+++ src/AuthServiceIN6BV.Application/Services/UuidGenerator.cs
@@ -0,0 +1,17 @@
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AuthService.Application.Services;
+
+public static class UuidGenerator
+{
+    private static readonly string Alphabet = "123456789ABCDEFGHJKMNPQRSTUVWXYZabcdefghijkmnpqrstuvwxyz";
+    public static GenerateShortUUID()
+    {
+        using var rng = RandomNumberGenerator.Create();
+        var bytes = new byte[12];
+        rng.GetBytes(bytes);
+
+        
+    }
+}
